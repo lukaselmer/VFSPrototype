56fe6c72d7e712bd635b8dc8c33466e05826526c
Lukas Elmer <lukas.elmer@gmail.com>
2013-03-19 01:31:56 +0100

    Initial commit
    Former-commit-id: d5ce4e561a21dea1c61b6e6fdc20fcd695be462a

---
+++ VFSBase/FileSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VFSBase
+{
+    public class FileSystem
+    {
+        public FileSystem(string location, long diskSize)
+        {
+            Location = location;
+            DiskSize = diskSize;
+
+            if (File.Exists(Location)) throw new VFSException("File already exists!");
+
+            File.WriteAllText(Location, "");
+
+            Root = new Folder();
+        }
+
+        public long DiskSize { get; private set; }
+
+        public string Location { get; private set; }
+
+        public Folder Root { get; private set; }
+
+        public void Destroy()
+        {
+            File.Delete(Location);
+        }
+    }
+}
---
+++ VFSBase/FileSystemManipulator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VFSBase
+{
+    public class FileSystemManipulator
+    {
+        private readonly FileSystem _fileSystem;
+
+        public FileSystemManipulator(FileSystem fileSystem)
+        {
+            _fileSystem = fileSystem;
+        }
+
+        public ISet<Folder> Folders
+        {
+            get { return _fileSystem.Root.Folders; }
+        }
+
+        public void CreateFolder(string path)
+        {
+            var folders = new Queue<string>(path.Split('/'));
+            _fileSystem.Root.CreateFolder(folders);
+        }
+
+        public bool DoesFolderExist(string path)
+        {
+            var folders = new Queue<string>(path.Split('/'));
+            return _fileSystem.Root.DoesFolderExist(folders);
+        }
+    }
+}
