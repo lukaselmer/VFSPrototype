1b8aeeb83adb6a4ecdae28ca8df0481e50132397
Lukas Elmer <lukas.elmer@gmail.com>
2013-03-25 23:11:06 +0100

    console application
    Former-commit-id: 2fd65c2d38cae0289e22760e85e6563a8e8ee1c9

---
+++ VFSConsole/ConsoleApplication.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace VFSConsole
+{
+    public class ConsoleApplication
+    {
+        private readonly TextReader _textReader;
+        private readonly TextWriter _textWriter;
+        private volatile bool _running = true;
+        private readonly IDictionary<string, Action<string>> _commands;
+
+        public ConsoleApplication(TextReader textReader, TextWriter textWriter)
+        {
+            _textReader = textReader;
+            _textWriter = textWriter;
+
+            _commands = new Dictionary<string, Action<string>>
+                            {
+                                {"help", ShowHelp},
+                                {"ls", ListDirectory},
+                                {"exit", Exit},
+                            };
+        }
+
+        private void Exit(string obj)
+        {
+            _running = false;
+        }
+
+        public void Run()
+        {
+            while (_running)
+            {
+                _textWriter.Write("> ");
+                var line = _textReader.ReadLine();
+                ProcessLine(line);
+            }
+            _textWriter.WriteLine("kthxbye");
+            _textReader.ReadLine();
+        }
+
+        private void ProcessLine(string line)
+        {
+            var commandAndArguments = line.Split(new[] { ' ' }, 2);
+            var command = commandAndArguments[0];
+            var arguments = commandAndArguments.Count() > 1 ? commandAndArguments[1] : "";
+
+            var func = _commands.ContainsKey(command) ? _commands[command] : CommandNotFound;
+            func(arguments);
+        }
+
+        private void ListDirectory(string parameter)
+        {
+            _textWriter.WriteLine("TODO: implement this!");
+            _textWriter.WriteLine("and this");
+            _textWriter.WriteLine("and so on...");
+        }
+
+        private void CommandNotFound(string parameter)
+        {
+            _textWriter.WriteLine("Sorry, your command was not found.");
+            ShowHelp("");
+        }
+
+        private void ShowHelp(string parameters)
+        {
+            _textWriter.WriteLine("Available commands:");
+            foreach (var command in _commands) _textWriter.WriteLine(command.Key);
+        }
+    }
+}
---
+++ VFSConsoleTests/ConsoleApplicationTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VFSConsole;
+
+namespace VFSConsoleTests
+{
+    [TestClass]
+    public class ConsoleApplicationTest
+    {
+        [TestMethod]
+        public void TestExit()
+        {
+            using (var mocks = new InOutMocks())
+            {
+                mocks.FakeInLine("exit", true);
+
+                var c = new ConsoleApplication(mocks.In, mocks.Out);
+                c.Run();
+                Assert.AreEqual("> kthxbye", mocks.FakeOutLine(true));
+            }
+        }
+
+        [TestMethod]
+        public void TestHelp()
+        {
+            using (var mocks = new InOutMocks())
+            {
+                mocks.FakeInLine("help");
+                mocks.FakeInLine("exit", true);
+
+                var c = new ConsoleApplication(mocks.In, mocks.Out);
+                c.Run();
+                Assert.AreEqual("> Available commands:", mocks.FakeOutLine(true));
+                Assert.AreEqual("help", mocks.FakeOutLine());
+                Assert.AreEqual("ls", mocks.FakeOutLine());
+                Assert.AreEqual("exit", mocks.FakeOutLine());
+            }
+        }
+
+        [TestMethod]
+        public void TestLs()
+        {
+            using (var mocks = new InOutMocks())
+            {
+                mocks.FakeInLine("ls");
+                mocks.FakeInLine("exit", true);
+
+                var c = new ConsoleApplication(mocks.In, mocks.Out);
+                c.Run();
+                Assert.AreEqual("> TODO: implement this!", mocks.FakeOutLine(true));
+                Assert.AreEqual("and this", mocks.FakeOutLine());
+                Assert.AreEqual("and so on...", mocks.FakeOutLine());
+            }
+        }
+    }
+}
