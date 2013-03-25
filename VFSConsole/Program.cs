1193d7bfd723ce07dd223629a6b30a3e067dcfb6
Lukas Elmer <lukas.elmer@gmail.com>
2013-03-25 23:11:06 +0100

    console application

---
+++ VFSConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VFSConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new ConsoleApplication(Console.In, Console.Out);
+            c.Run();
+        }
+    }
+}
---
+++ VFSConsoleTests/InOutMocks.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace VFSConsoleTests
+{
+    public class InOutMocks : IDisposable
+    {
+        private readonly MemoryStream _memoryIn;
+        private readonly MemoryStream _memoryOut;
+        private readonly StreamWriter _inWriter;
+        private readonly StreamReader _outReader;
+
+        public StreamWriter Out { get; private set; }
+
+        public StreamReader In { get; private set; }
+
+        public InOutMocks()
+        {
+            _memoryIn = new MemoryStream();
+            _inWriter = new StreamWriter(_memoryIn);
+            In = new StreamReader(_memoryIn);
+
+            _memoryOut = new MemoryStream();
+            _outReader = new StreamReader(_memoryOut);
+            Out = new StreamWriter(_memoryOut);
+        }
+
+        public void FakeInLine(string line, bool seekToBeginning = false)
+        {
+            _inWriter.WriteLine(line);
+            _inWriter.Flush();
+            if (seekToBeginning) _inWriter.BaseStream.Position = 0;
+        }
+
+        public void Dispose()
+        {
+            In.Dispose();
+            _inWriter.Dispose();
+            Out.Dispose();
+            _outReader.Dispose();
+            _memoryIn.Dispose();
+            _memoryOut.Dispose();
+        }
+
+        public string FakeOutLine(bool seekToBeginning = false)
+        {
+            Out.Flush();
+            if (seekToBeginning) _outReader.BaseStream.Position = 0;
+            return _outReader.ReadLine();
+        }
+    }
+}
