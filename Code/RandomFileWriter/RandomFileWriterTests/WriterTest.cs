da470ac89fc8a4d92993af273b9233a0a750f929
Lukas Elmer <lukas.elmer@gmail.com>
2013-04-02 18:11:16 +0200

    fix tests
    Former-commit-id: 3afb04cd56c356268731577b9e2e28901b33d7dc

--- Code/RandomFileWriter/RandomFileWriterTests/WriterTest.cs
+++ Code/RandomFileWriter/RandomFileWriterTests/WriterTest.cs
@@ -8,98 +8,154 @@ namespace RandomFileWriterTests
     [TestClass]
     public class WriterTest
     {
-        private const string Big = "../../../testfiles/bigfile.vhs";
+        private const string Big = "../../../testfiles/bigfile_";
         private const string Small = "../../../testfiles/small.vhs";
         private const string Same = "../../../testfiles/bigfile_same.vhs";
         private const string Bigger = "../../../testfiles/bigfile_bigger.vhs";
 
+        private static string RandomFilename()
+        {
+            return Big + Path.GetRandomFileName() + ".vhs";
+        }
+
         [TestMethod]
         public void TestRandomWriteBeginning()
         {
-            if (File.Exists(Big)) File.Delete(Big);
-            Assert.IsFalse(File.Exists(Big));
-
-            using (var w = new Writer(Big))
+            var filename = RandomFilename();
+            try
             {
-                w.WriteFile(Small, 0);
-            }
+                if (File.Exists(filename)) File.Delete(filename);
+                Assert.IsFalse(File.Exists(filename));
 
-            Assert.IsTrue(File.Exists(Big), String.Format("File {0} should exist", Big));
-            Assert.IsTrue(File.ReadAllText(Big).StartsWith("1234"));
+                using (var w = new Writer(filename))
+                {
+                    w.WriteFile(Small, 0);
+                }
+
+                Assert.IsTrue(File.Exists(filename), String.Format("File {0} should exist", filename));
+                Assert.IsTrue(File.ReadAllText(filename).StartsWith("1234"));
+            }
+            finally
+            {
+                File.Delete(filename);
+            }
         }
 
         [TestMethod]
         public void TestRandomWriteAfterBeginning()
         {
-            if (File.Exists(Big)) File.Delete(Big);
-            Assert.IsFalse(File.Exists(Big));
 
-            using (var w = new Writer(Big))
+            var filename = RandomFilename();
+
+            try
             {
-                w.WriteFile(Small, 1);
-            }
+                if (File.Exists(filename)) File.Delete(filename);
+                Assert.IsFalse(File.Exists(filename));
 
-            Assert.IsTrue(File.Exists(Big), String.Format("File {0} should exist", Big));
-            var x = File.ReadAllText(Big);
-            Assert.IsTrue(File.ReadAllText(Big).StartsWith("\01234"));
+                using (var w = new Writer(filename))
+                {
+                    w.WriteFile(Small, 1);
+                }
+
+                Assert.IsTrue(File.Exists(filename), String.Format("File {0} should exist", filename));
+                var x = File.ReadAllText(filename);
+                Assert.IsTrue(File.ReadAllText(filename).StartsWith("\01234"));
+            }
+            finally
+            {
+                File.Delete(filename);
+            }
         }
 
         [TestMethod]
         public void TestRandomWriteSomewhere()
         {
-            if (File.Exists(Big)) File.Delete(Big);
-            Assert.IsFalse(File.Exists(Big));
-
-            using (var w = new Writer(Big))
+            var filename = RandomFilename();
+            try
             {
-                w.WriteFile(Small, 3);
-            }
+                if (File.Exists(filename)) File.Delete(filename);
+                Assert.IsFalse(File.Exists(filename));
 
-            Assert.IsTrue(File.Exists(Big), String.Format("File {0} should exist", Big));
-            var x = File.ReadAllText(Big);
-            Assert.IsTrue(File.ReadAllText(Big).StartsWith("\0\0\01234"));
+                using (var w = new Writer(filename))
+                {
+                    w.WriteFile(Small, 3);
+                }
+
+                Assert.IsTrue(File.Exists(filename), String.Format("File {0} should exist", filename));
+                var x = File.ReadAllText(filename);
+                Assert.IsTrue(File.ReadAllText(filename).StartsWith("\0\0\01234"));
+            }
+            finally
+            {
+                File.Delete(filename);
+            }
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
         public void TestTooBigFile()
         {
-            if (File.Exists(Big)) File.Delete(Big);
-            Assert.IsFalse(File.Exists(Big));
-
-            using (var w = new Writer(Big))
+            var filename = RandomFilename();
+            try
             {
-                w.WriteFile(Bigger, 0); // this should throw an exception!
+                if (File.Exists(filename)) File.Delete(filename);
+                Assert.IsFalse(File.Exists(filename));
+
+                using (var w = new Writer(filename))
+                {
+                    w.WriteFile(Bigger, 0); // this should throw an exception!
+                }
+            }
+            finally
+            {
+                File.Delete(filename);
             }
         }
 
         [TestMethod]
         public void TestSmallerBigFile()
         {
-            if (File.Exists(Big)) File.Delete(Big);
-            Assert.IsFalse(File.Exists(Big));
+            var filename = RandomFilename();
 
-            using (var w = new Writer(Big))
+            try
             {
-                w.WriteFile(Same, 0); // this should just work
-            }
+                if (File.Exists(filename)) File.Delete(filename);
+                Assert.IsFalse(File.Exists(filename));
 
-            Assert.IsTrue(File.Exists(Big), String.Format("File {0} should exist", Big));
+                using (var w = new Writer(filename))
+                {
+                    w.WriteFile(Same, 0); // this should just work
+                }
+
+                Assert.IsTrue(File.Exists(filename), String.Format("File {0} should exist", filename));
+            }
+            finally
+            {
+                File.Delete(filename);
+            }
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
         public void TestSmallerBigFileFail()
         {
-            if (File.Exists(Big)) File.Delete(Big);
-            Assert.IsFalse(File.Exists(Big));
-
-            using (var w = new Writer(Big))
+            var filename = RandomFilename();
+            try
             {
-                w.WriteFile(Same, 1); // one byte overflow => this should throw an exception!
-            }
+                if (File.Exists(filename)) File.Delete(filename);
+                Assert.IsFalse(File.Exists(filename));
 
-            Assert.IsTrue(File.Exists(Big), String.Format("File {0} should exist", Big));
+                using (var w = new Writer(filename))
+                {
+                    w.WriteFile(Same, 1); // one byte overflow => this should throw an exception!
+                }
+
+                Assert.IsTrue(File.Exists(filename), String.Format("File {0} should exist", filename));
+            }
+            finally
+            {
+                File.Delete(filename);
+            }
         }
     }
 }
