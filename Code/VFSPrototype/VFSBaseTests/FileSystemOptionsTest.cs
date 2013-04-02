a57bb6dd87a716bf2964b207024e8419dd0b7be1
Lukas Elmer <lukas.elmer@gmail.com>
2013-04-02 03:32:25 +0200

    serialize system options
    Former-commit-id: 4bacedc7a3b056fafa2a3db2b5c51070a1c26972

---
+++ Code/VFSPrototype/VFSBaseTests/FileSystemOptionsTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VFSBase.Implementation;
+
+namespace VFSBaseTests
+{
+    [TestClass]
+    public class FileSystemOptionsTest
+    {
+        [TestMethod]
+        public void TestSerializeAndDeserialize()
+        {
+            using (var m = new MemoryStream())
+            {
+                var o1 = new FileSystemOptions("", 0);
+
+                o1.Size = 1001;
+                o1.MasterBlockSize = 30000;
+                o1.Serialize(m);
+
+                m.Seek(0, SeekOrigin.Begin);
+
+                var o2 = FileSystemOptions.Deserialize(m);
+                Assert.AreEqual(1001UL, o2.Size);
+                Assert.AreEqual(30000U, o2.MasterBlockSize);
+            }
+        }
+    }
+}
