988ff1a4b679e617275497c42fbb5d6ae62bebc6
Lukas Elmer <lukas.elmer@gmail.com>
2013-04-04 04:19:10 +0200

    fix kb and mb calculations
    Former-commit-id: 404cc966c6308b3fdf4bac58055744c58c593d85

--- Code/VFSPrototype/VFSBase/Persistence/BinaryMathUtil.cs
+++ Code/VFSPrototype/VFSBase/Persistence/BinaryMathUtil.cs
@@ -4,12 +4,12 @@
     {
         public static long MB(int i)
         {
-            return KB(1) * KB(1) * Power2(i);
+            return Power2(20) * i;
         }
 
         public static long KB(int i)
         {
-            return Power2(10);
+            return Power2(10) * i;
         }
 
         public static long Power2(int exponent)
