d929873e9827f9542aea5720b03fbe8e9ff43857
Lukas Elmer <lukas.elmer@gmail.com>
2013-04-04 04:09:56 +0200

    refactoring
    Former-commit-id: f031cca0a9da61034bb91902f7e538c8c07e9620

--- Code/VFSPrototype/VFSBase/Implementation/BlockList.cs
+++ Code/VFSPrototype/VFSBase/Implementation/BlockList.cs
@@ -12,23 +12,23 @@ namespace VFSBase.Implementation
         private readonly FileSystemOptions _options;
         private readonly BlockParser _blockParser;
         private readonly BlockManipulator _blockManipulator;
-        private readonly Persistence _persistance;
+        private readonly Persistence _persistence;
 
-        public BlockList(IIndexNode node, BlockAllocation blockAllocation, FileSystemOptions options, BlockParser blockParser, BlockManipulator blockManipulator, Persistence persistance)
+        public BlockList(IIndexNode node, BlockAllocation blockAllocation, FileSystemOptions options, BlockParser blockParser, BlockManipulator blockManipulator, Persistence persistence)
         {
             _node = node;
             _blockAllocation = blockAllocation;
             _options = options;
             _blockParser = blockParser;
             _blockManipulator = blockManipulator;
-            _persistance = persistance;
+            _persistence = persistence;
         }
 
         public void Add(long reference)
         {
             var parentFolder = _node as Folder;
-            
-            if(parentFolder == null) throw new NotImplementedException();
+
+            if (parentFolder == null) throw new NotImplementedException();
 
             var indirectNodeNumber = parentFolder.IndirectNodeNumber;
             if (indirectNodeNumber == 0)
@@ -44,25 +44,25 @@ namespace VFSBase.Implementation
             var indexIndirection0 = (int)(blocksCount - (indexIndirection2 * refsCount * refsCount) - (refsCount * indexIndirection1));
 
             parentFolder.BlocksCount += 1;
-            _persistance.PersistFolder(parentFolder);
+            _persistence.PersistFolder(parentFolder);
 
             var indirectNode3 = ReadIndirectNode(parentFolder.IndirectNodeNumber);
             if (indirectNode3.BlockNumbers[indexIndirection2] == 0)
             {
                 indirectNode3.BlockNumbers[indexIndirection2] = CreateIndirectNode().BlockNumber;
-                _persistance.Persist(indirectNode3);
+                _persistence.Persist(indirectNode3);
             }
 
             var indirectNode2 = ReadIndirectNode(indirectNode3.BlockNumbers[indexIndirection2]);
             if (indirectNode2.BlockNumbers[indexIndirection1] == 0)
             {
                 indirectNode2.BlockNumbers[indexIndirection1] = CreateIndirectNode().BlockNumber;
-                _persistance.Persist(indirectNode2);
+                _persistence.Persist(indirectNode2);
             }
 
             var indirectNode1 = ReadIndirectNode(indirectNode2.BlockNumbers[indexIndirection1]);
             indirectNode1.BlockNumbers[indexIndirection0] = reference;
-            _persistance.Persist(indirectNode1);
+            _persistence.Persist(indirectNode1);
         }
 
         private IndirectNode ReadIndirectNode(long reference)
@@ -74,8 +74,70 @@ namespace VFSBase.Implementation
         {
             var newNodeNumber = _blockAllocation.Allocate();
             var indirectNode = new IndirectNode(new long[_options.ReferencesPerIndirectNode]) { BlockNumber = newNodeNumber };
-            _persistance.Persist(indirectNode);
+            _persistence.Persist(indirectNode);
             return indirectNode;
+        }
+
+        public void Delete(long indirectNodeNumber)
+        {
+            var node = _node;
+
+            if (node.Parent.BlocksCount == 0) return;
+
+            node.Parent.BlocksCount--;
+            if (node.Parent.BlocksCount == 0)
+            {
+                node.Parent.IndirectNodeNumber = 0;
+                _persistence.PersistFolder(node.Parent);
+                return;
+            }
+
+            _blockManipulator.WriteBlock(node.Parent.BlockNumber, _blockParser.NodeToBytes(node.Parent));
+
+            var blocksCount = node.Parent.BlocksCount;
+            var refsCount = _options.ReferencesPerIndirectNode;
+
+            var indexIndirection2 = (int)(blocksCount / (refsCount * refsCount));
+            var indexIndirection1 = (int)((blocksCount - (indexIndirection2 * refsCount * refsCount)) / refsCount);
+            var indexIndirection0 = (int)(blocksCount - (indexIndirection2 * refsCount * refsCount) - (refsCount * indexIndirection1));
+
+            var indirectNode3 = ReadIndirectNode(node.Parent.IndirectNodeNumber);
+            var indirectNode2 = ReadIndirectNode(indirectNode3.BlockNumbers[indexIndirection2]);
+            var indirectNode1 = ReadIndirectNode(indirectNode2.BlockNumbers[indexIndirection1]);
+
+            var refToMove = indirectNode1.BlockNumbers[indexIndirection0];
+
+            // The file contents could be destroyed, but it is not necessary.
+            // Disabled, so "Move" can be implemented simpler.
+            // WriteBlock(node.BlockNumber, new byte[_options.BlockSize]);
+
+            indirectNode1.BlockNumbers[indexIndirection0] = 0;
+            _persistence.Persist(indirectNode1);
+
+            if (refToMove == node.BlockNumber) return;
+
+            ReplaceInIndirectNode(indirectNode3, node.BlockNumber, refToMove, 2);
+        }
+
+
+        private void ReplaceInIndirectNode(IndirectNode indirectNode, long toBeReplaced, long toReplace, int recursion)
+        {
+            for (var i = 0; i < indirectNode.BlockNumbers.Length; i++)
+            {
+                var blockNumber = indirectNode.BlockNumbers[i];
+                if (blockNumber == 0) return;
+
+                if (recursion == 0)
+                {
+                    if (blockNumber == toBeReplaced)
+                    {
+                        indirectNode.BlockNumbers[i] = toReplace;
+                        _persistence.Persist(indirectNode);
+                        return;
+                    }
+                }
+                else ReplaceInIndirectNode(ReadIndirectNode(blockNumber), toBeReplaced, toReplace, recursion - 1);
+            }
         }
     }
 }
