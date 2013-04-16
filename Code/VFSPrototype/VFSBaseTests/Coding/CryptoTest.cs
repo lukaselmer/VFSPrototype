0f32c2738df566b530177e203a918f1047cb12d1
Lukas Elmer <lukas.elmer@gmail.com>
2013-04-16 18:07:59 +0200

    test crypto algos
    Former-commit-id: c9f6056741e0771b6407e52c04c701d56dc1b548

---
+++ Code/VFSPrototype/VFSBaseTests/Coding/CryptoTest.cs
@@ -0,0 +1,85 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VFSBase.Persistence.Coding;
+
+namespace VFSBaseTests.Coding
+{
+    [TestClass]
+    public class CryptoTest
+    {
+        private static void TestAlgorithm(ICryptoTransform encryptor, ICryptoTransform decryptor)
+        {
+            var original = new byte[100000];
+            var result = new byte[100000];
+
+            new Random(1).NextBytes(original);
+
+            using (var ms = new MemoryStream())
+            {
+                var s = new CryptoStream(ms, encryptor, CryptoStreamMode.Write);
+                s.Write(original, 0, original.Length);
+                s.FlushFinalBlock();
+
+                ms.Seek(0, SeekOrigin.Begin);
+
+                var ss = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
+                var read = 0;
+                var pos = 0;
+                while ((pos = ss.Read(result, pos, original.Length - read)) > 0)
+                {
+                    read += pos;
+                }
+                Console.WriteLine(pos);
+            }
+
+            for (var i = 0; i < original.Length; i++)
+            {
+                Assert.AreEqual(original[i], result[i]);
+            }
+        }
+
+
+        private static EncryptionOptions GetEncryptionOptions()
+        {
+            using (var r = Rijndael.Create())
+            {
+                return new EncryptionOptions(r.Key, r.IV);
+            }
+        }
+
+        [TestMethod]
+        public void TestSelfMadeAesCryptor()
+        {
+            var options = GetEncryptionOptions();
+            TestAlgorithm(
+                new SelfMadeAesCryptor(options.Key, options.InitializationVector, SelfMadeAesCryptor.CryptoDirection.Encrypt),
+                new SelfMadeAesCryptor(options.Key, options.InitializationVector, SelfMadeAesCryptor.CryptoDirection.Decrypt));
+
+        }
+
+        [TestMethod]
+        public void TestSelfMadeSimpleCryptor()
+        {
+            var options = GetEncryptionOptions();
+            TestAlgorithm(
+                new SelfMadeSimpleCryptor(options.Key, options.InitializationVector, SelfMadeSimpleCryptor.CryptoDirection.Encrypt),
+                new SelfMadeSimpleCryptor(options.Key, options.InitializationVector, SelfMadeSimpleCryptor.CryptoDirection.Decrypt));
+
+        }
+
+
+        [TestMethod]
+        public void TestSelfMadeCaesarCryptor()
+        {
+            TestAlgorithm(
+                new SelfMadeCaesarCryptor(3, SelfMadeCaesarCryptor.CryptoDirection.Encrypt),
+                new SelfMadeCaesarCryptor(3, SelfMadeCaesarCryptor.CryptoDirection.Decrypt));
+
+            TestAlgorithm(
+                new SelfMadeCaesarCryptor(7, SelfMadeCaesarCryptor.CryptoDirection.Encrypt),
+                new SelfMadeCaesarCryptor(7, SelfMadeCaesarCryptor.CryptoDirection.Decrypt));
+        }
+    }
+}
