f2d0e49a1453c10440bd6cd04685ad193faf0d40
Ivo Nussbaumer <ivo.nussbaumer@gmail.com>
2013-04-19 00:40:40 +0200

    GUI Prototype with Delete/Move/Copy/Rename/Navigate/OpenVFS/NewVFS/Import/Export
    Former-commit-id: 4fa8073b6830273c6a9969e8a453f250bf339458 [formerly 1a49ce1fd0b0b2750b89e06a40cacbaeed04217d]
    Former-commit-id: bf693ff5aa383c280b9f946b88d2d6c3afa74a28

---
+++ Code/VFSPrototype/VFSBrowser/ViewModel/InputViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using VFSBrowser.View;
+
+namespace VFSBrowser.ViewModel
+{
+    public class InputViewModel : AbstractViewModel
+    {
+        private string _text;
+        public string Text { 
+            get { return _text; } 
+            set { 
+                _text = value;
+                OnPropertyChanged("Text");
+            } 
+        }
+
+        private string _label;
+        public string Label
+        {
+            get { return _label; }
+            set
+            {
+                _label = value;
+                OnPropertyChanged("Label");
+            }
+        }
+
+        public InputViewModel(string label, string text)
+        {
+            Label = label;
+            Text = text;
+        }
+
+        private InputDialog _dlg;
+        public bool? ShowDialog()
+        {
+            _dlg = new InputDialog(this);
+            return _dlg.ShowDialog();
+        }
+
+        private ICommand _okCommand;
+        public ICommand OkCommand { get { return _okCommand ?? (_okCommand = new Command(CloseDialog, p => (Text != ""))); } }
+
+        private void CloseDialog(object p)
+        {
+            _dlg.DialogResult = true;
+            _dlg.Close();
+        }
+    }
+}
---
+++ Code/VFSPrototype/VFSBrowser/ViewModel/ListItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VFSBrowser.ViewModel
+{
+    class ListItem : AbstractViewModel
+    {
+        private string _name;
+        public string Name { get { return _name; } set { _name = value; OnPropertyChanged("Name"); } }
+        private bool _isDirectory;
+        public bool IsDirectory { get { return _isDirectory; } set { _isDirectory = value; OnPropertyChanged("IsDirectory"); } }
+
+        public string Path { get; set; }
+
+        public ListItem(string path, string name, bool isDirectory)
+        {
+            IsDirectory = isDirectory;
+            Path = path;
+            Name = name;
+        }
+    }
+}
