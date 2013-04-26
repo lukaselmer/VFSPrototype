5f9162dc894e78b0602843dd787ebf0a35940a75
Lukas Elmer <lukas.elmer@gmail.com>
2013-04-27 01:04:17 +0200

    doc
    Former-commit-id: 603d2377b94161331059786f653a111c7ea1dd23

--- Code/VFSPrototype/VFSBase/Persistence/PositionCalculator.cs
+++ Code/VFSPrototype/VFSBase/Persistence/PositionCalculator.cs
@@ -1,24 +1,51 @@
 ﻿namespace VFSBase.Persistence
 {
+    /// <summary>
+    /// The position calculator
+    /// </summary>
     internal class PositionCalculator
     {
+        /// <summary>
+        /// The settings
+        /// </summary>
         private readonly PositionCalculatorSettings _settings;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PositionCalculator"/> class.
+        /// </summary>
+        /// <param name="positionCalculatorSettings">The position calculator settings.</param>
         public PositionCalculator(PositionCalculatorSettings positionCalculatorSettings)
         {
             _settings = positionCalculatorSettings;
         }
 
+        /// <summary>
+        /// Gets the settings.
+        /// </summary>
+        /// <value>
+        /// The settings.
+        /// </value>
         public PositionCalculatorSettings Settings
         {
             get { return _settings; }
         }
 
+        /// <summary>
+        /// Gets the start block.
+        /// </summary>
+        /// <value>
+        /// The start block.
+        /// </value>
         public long StartBlock
         {
             get { return Settings.SuperBlockSize; }
         }
 
+        /// <summary>
+        /// Calculates the block start.
+        /// </summary>
+        /// <param name="position">The position.</param>
+        /// <returns></returns>
         public long CalculateBlockStart(long position)
         {
             return StartBlock + position * Settings.BlockSize;
