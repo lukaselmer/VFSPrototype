e0a7bec7d3c79aa3ba2f363adceae3c5ca7c7f63
Lukas Elmer <lukas.elmer@gmail.com>
2013-04-27 01:01:40 +0200

    code doc
    Former-commit-id: be9a4ede024fd904b2e6a2d3626140486e7b38f3

--- Code/VFSPrototype/VFSBase/Persistence/Coding/General/NullStreamCodingStrategy.cs
+++ Code/VFSPrototype/VFSBase/Persistence/Coding/General/NullStreamCodingStrategy.cs
@@ -9,11 +9,21 @@ namespace VFSBase.Persistence.Coding.General
     /// </summary>
     public class NullStreamCodingStrategy : IStreamCodingStrategy
     {
+        /// <summary>
+        /// Decorates the steam, so it can be written to the virtual file system.
+        /// </summary>
+        /// <param name="stream">The stream.</param>
+        /// <returns></returns>
         public Stream DecorateToVFS(Stream stream)
         {
             return stream;
         }
 
+        /// <summary>
+        /// Decorates the steam, so it can be written to the host system.
+        /// </summary>
+        /// <param name="stream">The stream.</param>
+        /// <returns></returns>
         public Stream DecorateToHost(Stream stream)
         {
             return stream;
