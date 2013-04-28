1f00f7254fb79afd4ed9bbf4340c09d6d8d6b3a7
Lukas Elmer <lukas.elmer@gmail.com>
2013-04-28 05:25:18 +0200

    tests

---
+++ Code/VFSPrototype/VFSBase/Implementation/SelfMadeLz77StreamCompressionStrategy.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.IO.Compression;
+using VFSBase.Persistence.Coding.General;
+
+namespace VFSBase.Implementation
+{
+    internal class SelfMadeLz77StreamCompressionStrategy : IStreamCodingStrategy
+    {
+        public Stream DecorateToVFS(Stream stream)
+        {
+            return new SelfMadeLz77Stream(stream, CompressionMode.Compress);
+        }
+
+        public Stream DecorateToHost(Stream stream)
+        {
+            return new SelfMadeLz77Stream(stream, CompressionMode.Decompress);
+        }
+    }
+}
---
+++ Code/VFSPrototype/VFSBaseTests/Coding/CompressionTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VFSBase.Implementation;
+
+namespace VFSBaseTests.Coding
+{
+    [TestClass]
+    public class CompressionTest
+    {
+        [TestMethod]
+        public void TestLz77()
+        {
+            var r = new Random(1);
+            var expected = new byte[10000];
+            var actual = new byte[10000];
+            r.NextBytes(expected);
+
+            using (var ms = new MemoryStream())
+            {
+                using (var s = new SelfMadeLz77Stream(ms, CompressionMode.Compress))
+                {
+                    var pos = 0;
+                    while (pos < expected.Length)
+                    {
+                        var count = r.Next(1024);
+                        s.Write(expected, pos, count);
+                        pos += count;
+                    }
+                }
+
+                ms.Position = 0;
+
+                using (var s = new SelfMadeLz77Stream(ms, CompressionMode.Decompress))
+                {
+                    var pos = 0;
+                    while (pos < expected.Length)
+                    {
+                        var count = r.Next(1024);
+                        s.Read(actual, pos, count);
+                        pos += count;
+                    }
+                }
+
+                for (var i = 0; i < expected.Length; i++)
+                {
+                    Assert.AreEqual(expected[i], actual[i]);
+                }
+            }
+        }
+    }
+}
