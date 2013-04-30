419ed7567a50226361053664ed8aaf5631724e03
Lukas Elmer <lukas.elmer@gmail.com>
2013-04-30 17:53:20 +0200

    scaffold

---
+++ Code/VFSPrototype/VFSWCFServer/IUserService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace VFSWCFServer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IUserService" in both code and config file together.
+    [ServiceContract]
+    public interface IUserService
+    {
+        [OperationContract]
+        User Register(string login, string hashedPassword);
+
+        [OperationContract]
+        User Login(string login, string hashedPassword);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class User
+    {
+        [DataMember]
+        public string Login { get; set; }
+
+        [DataMember]
+        public string HashedPassword { get; set; }
+    }
+}
---
+++ Code/VFSPrototype/VFSWCFServerTests/UserServiceTest.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VFSWCFService;
+
+namespace VFSWCFServiceTests
+{
+    [TestClass]
+    public class UserServiceTest
+    {
+        [TestMethod]
+        public void TestRegistration()
+        {
+            var persistence = new Persistence();
+            var s = new UserService { Persistence = persistence };
+            var user = s.Register("bla", "blub");
+            Assert.AreEqual("bla", user.Login);
+            Assert.AreEqual("blub", user.HashedPassword);
+            Assert.IsTrue(persistence.Exists("bla"));
+        }
+
+        [TestMethod]
+        public void TestRegistrationFail()
+        {
+            var persistence = new Persistence();
+            persistence.CreateUser("bla", "test");
+            var s = new UserService { Persistence = persistence };
+            var user = s.Register("bla", "blub");
+            Assert.IsNull(user);
+        }
+
+        [TestMethod]
+        public void TestLogin()
+        {
+            var persistence = new Persistence();
+            persistence.CreateUser("bla", "blub");
+            var s = new UserService { Persistence = persistence };
+            var user = s.Login("bla", "blub");
+            Assert.AreEqual("bla", user.Login);
+            Assert.AreEqual("blub", user.HashedPassword);
+        }
+
+        [TestMethod]
+        public void TestLoginFail()
+        {
+            var persistence = new Persistence();
+            persistence.CreateUser("bla", "test");
+            var s = new UserService { Persistence = persistence };
+            var user = s.Login("bla", "blub");
+            Assert.IsNull(user);
+        }
+    }
+}
