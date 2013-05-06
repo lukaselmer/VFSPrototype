b8f3d8ea8cb81d7fc8f9c622d58998d77ab26954
Lukas Elmer <lukas.elmer@gmail.com>
2013-05-06 19:22:50 +0200

    enhance service

---
+++ Code/VFSPrototype/VFSWCFServer/DiskService/DiskOptions.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace VFSWCFService.DiskService
+{
+    /// <summary>
+    /// The disk options are used to create the disk on the server and on the client (initial synchronization).
+    /// </summary>
+    [DataContract]
+    public class DiskOptions
+    {
+        [DataMember]
+        int BlockSize { get; set; }
+
+        [DataMember]
+        int MasterBlockSize { get; set; }
+
+        [DataMember]
+        byte[] SerializedFileSystemOptions { get; set; }
+    }
+}
