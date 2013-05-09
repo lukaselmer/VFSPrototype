17a2dee99bad2105497b92662f971124a5040156
Ivo Nussbaumer <ivo.nussbaumer@gmail.com>
2013-05-09 14:44:58 +0200

    Fixed Search/Index/Icons
    Former-commit-id: b2d373a4ef70de8d3c3a2d382f8cfb48cb884e41

--- Code/VFSPrototype/VFSBase/Search/IndexService.cs
+++ Code/VFSPrototype/VFSBase/Search/IndexService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,27 +29,56 @@ namespace VFSBase.Search
         {
             var result = _flatNames.Search(searchOptions);
 
-            if (searchOptions.RestrictToFolder != null)
-                return result.Where (indexNode => IsInFolder (searchOptions.RestrictToFolder, indexNode, searchOptions.RecursionDistance));
-            
-            return result;
+            var restrictFolderPath = (searchOptions.RestrictToFolder == null) 
+                                     ? "" 
+                                     : GetPath(searchOptions.RestrictToFolder);
+                
+            return result.Where(indexNode => IsInFolder(restrictFolderPath, GetPath(indexNode), searchOptions.RecursionDistance));
         }
 
-        private static bool IsInFolder(Folder folder, IIndexNode node, int recursionDistance)
+        private static string GetPath(IIndexNode node)
         {
-            var parent = node.Parent;
-
-            var recursionDepth = 0;
-            while (parent != null && (recursionDistance == -1 || recursionDepth <= recursionDistance))
+            var sb = new StringBuilder();
+            
+            while (node != null)
             {
-                if (parent.Equals(folder))
+                sb.Insert(0, node.Name + "/");
+                node = node.Parent;
+            }
+            return sb.ToString().TrimEnd('/');
+        }
+
+        private static bool IsInFolder (string restrictFolderPath, string nodePath, int recursionDistance)
+        {
+            var recursionDepth = 0;
+            while ((recursionDistance == -1 || recursionDepth <= recursionDistance) && nodePath.Length > 0)
+            {
+                nodePath = nodePath.Substring (0, nodePath.LastIndexOf ("/", StringComparison.CurrentCulture));
+
+                if (nodePath == restrictFolderPath)
                     return true;
 
                 recursionDepth++;
-                parent = parent.Parent;
             }
+                
             return false;
-        }
+        } 
+
+        //private static bool IsInFolder(Folder folder, IIndexNode node, int recursionDistance)
+        //{
+        //    var parent = node.Parent;
+
+        //    var recursionDepth = 0;
+        //    while (parent != null && (recursionDistance == -1 || recursionDepth <= recursionDistance))
+        //    {
+        //        if (parent.Equals(folder))
+        //            return true;
+
+        //        recursionDepth++;
+        //        parent = parent.Parent;
+        //    }
+        //    return false;
+        //}
 
         internal void AddToIndex(IIndexNode node)
         {
