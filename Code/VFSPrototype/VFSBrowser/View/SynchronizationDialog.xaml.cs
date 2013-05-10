353c68fb38f42c7f41984f84ef3f56dfec86594d
Lukas Elmer <lukas.elmer@gmail.com>
2013-05-10 23:00:07 +0200

    gui for sync progress
    Former-commit-id: 8042f3c3d64c821c2d60b4aee999766421bf8a25

--- Code/VFSPrototype/VFSBrowser/View/SynchronizationDialog.xaml.cs
+++ Code/VFSPrototype/VFSBrowser/View/SynchronizationDialog.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,18 +12,40 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using VFSBrowser.ViewModel;
 
 namespace VFSBrowser.View
 {
     /// <summary>
     /// Interaction logic for SynchronizationDialog.xaml
     /// </summary>
-    public partial class SynchronizationDialog : Window
+    internal partial class SynchronizationDialog : Window
     {
-        public SynchronizationDialog(object viewModel)
+        readonly string[] _images = new[] { 
+            "http://imgs.xkcd.com/comics/flowchart.png",
+            "http://imgs.xkcd.com/comics/bonding.png",
+            "http://imgs.xkcd.com/comics/ineffective_sorts.png", 
+            "http://imgs.xkcd.com/comics/encoding.png", 
+            "http://imgs.xkcd.com/comics/footnote_labyrinths.png",
+            "http://imgs.xkcd.com/comics/einstein.png",
+            "http://imgs.xkcd.com/comics/integration_by_parts.png",
+            "http://imgs.xkcd.com/comics/estimation.png"
+        };
+
+        private int _imageIndex;
+        private readonly SynchronizationViewModel _viewModel;
+
+        public SynchronizationDialog(SynchronizationViewModel viewModel)
         {
             InitializeComponent();
-            DataContext = viewModel;
+            DataContext = _viewModel = viewModel;
+        }
+
+        private void ProhibitClosing(object sender, CancelEventArgs e)
+        {
+            e.Cancel = !_viewModel.Closed;
+            Comic.Source = new BitmapImage(new Uri(_images[_imageIndex++ % _images.Length]));
+            Explanation.Content = "Sorry, synchronization cannot be aborted, it's too dangerous";
         }
     }
 }
