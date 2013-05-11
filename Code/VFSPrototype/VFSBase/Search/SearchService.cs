bd9e555ac20c0b8096bc526e31c63149090cbf49
Lukas Elmer <lukas.elmer@gmail.com>
2013-05-11 20:41:20 +0200

    fixed search service

--- Code/VFSPrototype/VFSBase/Search/SearchService.cs
+++ Code/VFSPrototype/VFSBase/Search/SearchService.cs
@@ -20,29 +20,31 @@ namespace VFSBase.Search
 
         public void StartIndexing()
         {
+            Index("/"); // See comment below
             // Does not work (yet)... Low prio...? 
             //Index("/");
             //Task.Run(() => Index("/"));
-            new Thread (ThreadDelegate).Start ("/");
+            //new Thread(ThreadDelegate).Start("/");
         }
 
-        private void ThreadDelegate (object o)
+        private void ThreadDelegate(object o)
         {
             Index(o.ToString());
         }
 
         private void Index(string path)
         {
+            path = path.TrimEnd('/');
             lock (_lock)
             {
                 foreach (var file in _manipulator.Files(path))
                 {
-                    _indexService.AddToIndex(path + file);
+                    _indexService.AddToIndex(path + "/" + file);
                 }
                 foreach (var folder in _manipulator.Folders(path.TrimEnd('/')))
                 {
-                    _indexService.AddToIndex (path + folder + "/");
-                    Index (path + folder + "/");
+                    _indexService.AddToIndex(path + "/" + folder + "/");
+                    Index(path + "/" + folder);
                 }
             }
         }
@@ -65,6 +67,21 @@ namespace VFSBase.Search
 
         public void AddToIndexRecursive(string path)
         {
+            // Tests didn't run... TODO: fix this (and run tests after fix)
+            //
+            // About concurrency: this cannot work concurrently!
+            // Scenario:
+            // T1: Create folder "a"
+            // T1: Create folder "b"
+            // T2: Start indexing "a"
+            // T1: Delete folder "a"
+            // T2: Start indexing contents of "a" => "a" deleted => no contents => exception!
+            //
+            // Possible solutions:
+            //  1) Do it synchonously
+            //  2) Do it only when searching
+            //  3) Use locking in the fileSystemTextManipulator
+
             if (!_manipulator.Exists(path)) return;
 
             AddToIndex(path);
@@ -72,8 +89,8 @@ namespace VFSBase.Search
             // Does not work (yet)... Low prio...? 
             // if (_manipulator.IsDirectory(path.TrimEnd('/'))) Index(path.TrimEnd('/'));
             // if (_manipulator.IsDirectory(path)) Task.Run(() => Index(path));
-            if (_manipulator.IsDirectory (path.TrimEnd ('/')))
-                new Thread (ThreadDelegate).Start (path);
+            if (_manipulator.IsDirectory(path.TrimEnd('/'))) Index(path);
+            //new Thread(ThreadDelegate).Start(path);
         }
     }
 }
